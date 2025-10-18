834b04bfd7358bb5ae5038be37e36dcfe9ca93cc
khal_huntsman <hunter.jude.steele@gmail.com>
2025-10-18 01:12:09 -0500

    made changes to index.js as per lab instructions

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-
-
+function combineUsers(...args) {
+  // initialize the return object
+  const combinedObject = {
+    users: []
+  };
+// loop through each array and merge using spread operator
+  for (let userArray of args) {
+    combinedObject.users.push(...userArray);
+  }
+ // get today's date in "M/d/yyyy" format
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+  // return the combined object
+  return combinedObject
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
